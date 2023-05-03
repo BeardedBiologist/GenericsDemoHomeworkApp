166acb50a4f2e2c9060dfebdda9e65a6c51149a7
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-03 13:07:14 +0200

    init commit. Finished homework.

--- GenericsDemoHomework/Program.cs
+++ GenericsDemoHomework/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace GenericsDemoHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 42;
+            string myString = "Hello, world!";
+
+            PrintItemToString(myInt);
+            PrintItemToString(myString);
+        }
+
+        static void PrintItemToString<T>(T item)
+        {
+            Console.WriteLine(item.ToString());
+        }
+    }
+}
